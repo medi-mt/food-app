1ddd3961ba87b730aa6ed40564e4d0dcbff4cf80
mehdi <mehdimoazemi1919@gmail.com>
2025-09-22 17:37:31 +0330

    getStaticPaths-&-getStaticProps-in-SSG

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,6 +1,32 @@
+import Link from "next/link";
 
-export default function Users() {
+export default function Users(props) {
+    const { data } = props
     return (
-        <div>Users</div>
+        <div>
+            <ul style={{ listStyle: "none", color: "red" }}>
+                {data.map(user =>
+                    <Link href={`/users/${user.id}`}>
+                        <li key={user.id}>
+                            {user.firstName}
+                        </li>
+                    </Link>
+                )}
+            </ul>
+        </div>
     )
+}
+
+
+export async function getStaticProps() {
+
+    const data = await fetch("https://dummyjson.com/users");
+    const users = await data.json()
+
+    return {
+        props: {
+            data: users.users
+        }
+    }
+
 }
